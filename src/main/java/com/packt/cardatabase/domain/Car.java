2c95950a943963e457e7119b5e479f1d5ef72edb
hhaemin <ahrgoals@naver.com>
2024-10-20 23:16:24 +0900

    :zap: Add Entity

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,7 +1,83 @@
 package com.packt.cardatabase.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // 데이터베이스가 자동으로 ID를 생성하도록 지정
+    private Long id;
+    private String brand, model, color, registerNumber;
+    @Column(name = "`year`")
+    private int year;
+    private int price;
+
+    public Car() {}
+
+    // 자동으로 ID를 생성하도록 설정해 둔 덕분에 생성자에 ID 필드는 필요없다.
+    public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+        super();
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
