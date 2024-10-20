b118232f04422276397811a6281ff87435bd5239
hhaemin <ahrgoals@naver.com>
2024-10-21 00:13:59 +0900

    :package: 쿼리 정의

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,5 +2,15 @@ package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CarRepository extends CrudRepository<Car, Long> {
+    // 브랜드로 자동차 검색
+    List<Car> findByBrand(String brand);
+
+    // 색상으로 자동차 검색
+    List<Car> findByColor(String color);
+
+    // 연도로 자동차 검색
+    List<Car> findByYear(int year);
 }
