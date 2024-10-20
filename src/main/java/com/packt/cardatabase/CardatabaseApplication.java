1b5329b1b005adf08c96be75c2de1da03bbb0884
hhaemin <ahrgoals@naver.com>
2024-10-20 22:12:25 +0900

    :tada: Maven + SpringBoot

---
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.packt.cardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+
+	public static void main(String[] args) {
+		// 이 주석을 추가하면 애플리케이션이 재시작됨.
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+}
