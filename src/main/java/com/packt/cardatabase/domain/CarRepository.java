e4598a161d8c2f013b0d5cf9dbe74346f69a671d
hhaemin <ahrgoals@naver.com>
2024-10-28 10:50:54 +0900

    서비스 레포지토리에 대한 쿼리 추가

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,17 +1,19 @@
 package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 //@RepositoryRestResource(path = "vehicles")
 public interface CarRepository extends CrudRepository<Car, Long> {
     // 브랜드로 자동차 검색
-    List<Car> findByBrand(String brand);
+    List<Car> findByBrand(@Param("brand") String brand);
 
     // 색상으로 자동차 검색
-    List<Car> findByColor(String color);
+    List<Car> findByColor(@Param("color") String color);
 
     // 연도로 자동차 검색
     List<Car> findByYear(int year);
